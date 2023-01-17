e76f71bc7e39b3711d2fc46039f153aa1dc1da5d
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-01-17 17:17:33 -0300

    style:minor style changes

--- src/controllers/negotiation-controller.ts
+++ src/controllers/negotiation-controller.ts
@@ -1,6 +1,7 @@
 import { Negotiation } from "../models/negotiation.js";
 import { Negotiations } from "../models/negotiations.js";
-import { NegotiationsView } from "../views/negociations-view.js";
+import { MessageView } from "../views/message-view.js";
+import { NegotiationsView } from "../views/negotiations-view.js";
 
 export class NegotiationController {
   private _inputDate: HTMLInputElement;
@@ -8,6 +9,7 @@ export class NegotiationController {
   private _inputValue: HTMLInputElement;
   private _negotiations = new Negotiations();
   private _negotationsView = new NegotiationsView("#table-container");
+  private _messageView = new MessageView("#mensagemView");
 
   constructor() {
     this._inputDate = <HTMLInputElement>document.getElementById("data");
@@ -17,7 +19,7 @@ export class NegotiationController {
     this._inputValue = <HTMLInputElement>document.getElementById("valor");
 
     //Using the template method of the view to render the table once the controller is created
-    this._negotationsView.updateView(this._negotiations);
+    this._negotationsView.update(this._negotiations);
   }
 
   cleanForm(): void {
@@ -41,6 +43,8 @@ export class NegotiationController {
     this.cleanForm();
 
     //Saying to the view to update everytime we add a new negotiation with the negotiations(model) as a parameter
-    this._negotationsView.updateView(this._negotiations);
+    this._negotationsView.update(this._negotiations);
+    this._messageView.update("Transaction added successfully");
+    setTimeout(() => this._messageView.clearMesasage(), 3000);
   }
 }
