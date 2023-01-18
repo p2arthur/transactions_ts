5e7b09a9778bf32d13b75b862f6bbb9e13da0653
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-01-18 17:52:41 -0300

    refactor: refactor the week-days enum to have a index

--- src/enums/week-days.ts
+++ src/enums/week-days.ts
@@ -1,11 +1,11 @@
 //Creating a enum data structure to hold all weekdays that will be used across the application
 
 export enum WeekDays {
-  SUNDAY,
-  MONDAY,
-  TUESDAY,
-  WEDNESDAY,
-  THURSDAY,
-  FRIDAY,
-  SATURDAY,
+  SUNDAY = 0,
+  MONDAY = 1,
+  TUESDAY = 2,
+  WEDNESDAY = 3,
+  THURSDAY = 4,
+  FRIDAY = 5,
+  SATURDAY = 6,
 }
