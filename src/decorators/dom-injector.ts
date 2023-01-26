b3c5e510d9f412256f0b9c680d3e88545d55316e
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-01-26 20:49:40 -0300

    refactor: implement second part of the domInjector decorator

--- src/decorators/dom-injector.ts
+++ src/decorators/dom-injector.ts
@@ -1,4 +1,9 @@
 //Creating a new decorator to return the element from a document.querySelector("selector")
 export function domInjector(selector: string) {
-  return function (target: any, propertyKey: string) {};
+  return function (target: any, propertyKey: string) {
+    const getter = function () {
+      const element = document.querySelector(selector);
+      return element;
+    };
+  };
 }
